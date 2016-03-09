72acdacea1f3a209afeb7f7cdc1e77ccbf346280
Serguey Arellano <sarellano85@gmail.com>
2016-03-09 20:58:53 +0100

    add questions file

--- questions/functional_questions.js
+++ questions/functional_questions.js
@@ -1,53 +1,7 @@
-/* • 
-
-Programacion funcional
-
-• Su creación es debida a las necesidades de los
-investigadores en el campo de la inteligencia
-artificial y en sus campos secundarios del cálculo
-simbólico, pruebas de teoremas, sistemas basados
-en reglas y procesamiento del lenguaje natural.
-
-• La característica principal de la programación funcional es
-que los cálculos se ven como una función matemática que
-hacen corresponder entradas y salidas.
-
-• No hay noción de posición de memoria y por tanto,
-necesidad de una instrucción de asignación.
-
-• Los bucles se modelan a través de la recursividad ya que
-no hay manera de incrementar o disminuir el valor de una
-variable.
-
-• Como aspecto práctico casi todos los lenguajes funcionales
-soportan el concepto de variable, asignación y bucle.
-
-• Estos elementos no forman parte del modelo funcional
-“puro”.
-
-
-Currificación
--------------
-Las funciones con más de un argumento se pueden interpretar
-como funciones que toman un único argumento y devuelven como
-resultado otra función con un argumento menos.
-Este mecanismo que permite representar funciones de varios
-argumentos mediante funciones de un argumento se denomina
-“CURRIFICACIÓN” (debido a Haskell B. Curry)
-
-Evaluación perezosa
--------------------
-
-Una expresión no se evalúa hasta que realmente se necesita.
-
-En javascript existe evaluacion perezosa cuando utilizamos los operadores && y ||
-
-http://notasjs.blogspot.com.es/2013/02/evaluacion-perezosa-de-operadores-y.html
-
-
+/*
 ┌─────────────────────────┐
 │ #01 Recursividad lineal │
-┕━━━━━━━━━━━━━━━━━━━━━━━━━┙
+└─────────────────────────┘
 
 "Write a program that prints if a number is even or not"
 */
@@ -58,12 +12,18 @@ function isEven(n) {
 		!isEven(n-1);
 }
 
-// console.log(isEven(101));
+if (true) {
+
+	console.log('isEven(1)', isEven(1));
+	console.log('isEven(24)', isEven(24));
+	console.log('isEven(11)', isEven(11));
+	console.log('----------------------');
+}
 
 /*
 ┌───────────────────────────────────────────────────┐
 │ #02 Recursividad lineal, con evaluacion perezosa. │
-┕━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┙
+└───────────────────────────────────────────────────┘
 
 "Write a program that prints the numbers from 1 to 100. 
 But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. 
@@ -80,12 +40,16 @@ function fizzBuzzAux(n, p) {
 		((p % 5 === 0 && p % 3 === 0 && 'FizzBuzz') || (p % 5 === 0 && 'Buzz') || (p % 3 === 0 && 'Fizz') || p) + '\n' + fizzBuzzAux(n, p + 1);
 }
 
-// console.log(fizzBuzz(100));
+if (false) {	
+	console.log('fizzBuzz(30) \n' + fizzBuzz(30));
+	console.log('----------------------');
+
+}
 
 /*
 ┌───────────────────────────┐
 │ #03 Recursividad multiple	│
-┕━━━━━━━━━━━━━━━━━━━━━━━━━━━┙
+└───────────────────────────┘
 
 "Write a program that prints the number located at a position in the fibonacci series"
 */
@@ -100,7 +64,7 @@ function fibonacci(n) {
 
 // Optimization with memoization
 
-var fibonacci = function fibonacci() {
+var fibonacciMemo = function fibonacciMemo() {
 	var memo = [0, 1];
 	var fib = function fib (n) {
 		var result = memo[n];
@@ -118,7 +82,7 @@ var fibonacci = function fibonacci() {
 /*
 ┌─────────────────────────────────────┐
 │ #04 Recursividad multiple, con cola │
-┕━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┙
+└─────────────────────────────────────┘
 
 Write a program that prints a compressed string.
 If the string is 'aabccc' the output should be 'a2bc3'
@@ -143,7 +107,7 @@ function compressAux(s, p, ac) {
 /*
 ┌────────────┐
 │ #05 Reduce │
-┕━━━━━━━━━━━━┙
+└────────────┘
 */
 function reduce (v, fn, o) {
 	return reduceAux(v, fn, o, 0);
@@ -154,3 +118,4 @@ function reduceAux(v, fn, o, p) {
 		o :
 		reduceAux(v, fn, fn(o, v[p], p, v), p + 1);
 }
+
